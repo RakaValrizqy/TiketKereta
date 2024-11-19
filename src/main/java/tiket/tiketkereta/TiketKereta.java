f5716930a8552e79ee9c582b3f27f9f76028b3d1
RakaValrizqy <myemail>
2024-11-19 18:28:22 +0700

    first init

---
+++ src/main/java/tiket/tiketkereta/TiketKereta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tiket.tiketkereta;
+
+/**
+ *
+ * @author RVA
+ */
+public class TiketKereta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
